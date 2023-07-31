32d29f6fef0746fed85698e3554e1628ae6ab7cd
Luong Thi Phuong Chi <luongphuongchi1505@gmail.com>
2023-07-31 22:41:24 +0700

    updated

---
+++ Assignment/exe-following-links.py
@@ -0,0 +1,26 @@
+import urllib.request, urllib.parse, urllib.error
+import collections
+collections.Callable = collections.abc.Callable
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter URL:')
+count = input('Enter count:')
+position = input('Enter position:')
+
+print('Retrieving:', url)
+
+for i in range(0,int(count)):
+    html = urllib.request.urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # Retrieve tags
+    tags = soup('a',limit=int(position))
+    for tag in tags:
+        url = tag.get('href',None)
+    print('Retrieving:', tag.get('href',None))
